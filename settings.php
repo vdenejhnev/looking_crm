190471c7714725ec32d46364aa51bbf1cbfeaaee
vdenezhnev <slavikgolos@gmail.com>
2023-04-08 17:38:42 +0300

    create project

--- settings.php
+++ settings.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 
 // для подключения к базе данных
 $DB_SERVER   =   "localhost";
-$DB_USER     =   "a0443796_tech";
-$DB_PASSWORD =   "IUv7aOpC";
-$DB_NAME     =   "a0443796_tech";
+$DB_USER     =   "root";
+$DB_PASSWORD =   "root";
+$DB_NAME     =   "taxi-trust";
 
 // полное имя сайта
 $SERVER      =   "//$_SERVER[HTTP_HOST]/";
